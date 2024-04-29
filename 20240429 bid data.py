846242e6fdc1f44399e7ce39aa6f16fbaf692a22
sam <166351894+samxinliang@users.noreply.github.com>
2024-04-29 11:40:51 +0800

    Add files via upload

--- 20240429 bid data.py
+++ 20240429 bid data.py
@@ -15,5 +15,26 @@ for i in range(1,100):
         odd += 1
 print('\neven = %d, odd = %d'%(even,odd))
 
-        
+import random
+times3 = 0
+times5 = 0
+times7 = 0
+others = 0
+for i in range(1,101):
+  flag = False
+  randNum = random.randint(1,100)
+  print(randNum, end = '  ')
+  if randNum % 3 == 0:
+    times3 += 1
+    flag = True
+  if randNum % 5 == 0:
+    times5 += 1
+    flag = True
+  if randNum % 7 == 0:
+    times7 +=1
+    flag = True
+  if flag == False:
+    others += 1
+print('\ntimes3 = %d, times5 = %d,times7 = %4d'%(times3,times5,times7))
+print('other = %d'%(others))
     
